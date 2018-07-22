404c97bc76978dcb4bfa69019aa18f188b6c9578
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-07-22 17:14:25 -0300

    :sparkles: add docker ce

---
+++ cli/commands/docker.sh
@@ -0,0 +1,14 @@
+# Installs Docker Community Edition
+
+izi get apt-transport-https
+izi get ca-certificates
+izi get curl
+izi get software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+izi up
+izi get docker-ce
