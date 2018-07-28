73f3f0a584ef40e4af5f7d1047f369eae9e28197
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-07-28 14:04:50 -0300

    :sparkles: add chrome

---
+++ cli/commands/chrome.sh
@@ -0,0 +1,4 @@
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+izi up
+izi get google-chrome-stable
