3cc7d78079cba6caf7795aa335d9bc86067936c6
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-07-29 13:59:09 -0300

    :zap: add fix for watcher in code install

--- cli/commands/code.sh
+++ cli/commands/code.sh
@@ -6,4 +6,5 @@ sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 izi up
 izi get code
-echo "alias vs='code . --disable-gpu'" >> ~/.bashrc
+echo "alias vs='code . --disable-gpu'" >> $HOME/.bashrc
+sudo echo "fs.inotify.max_user_watches=524288" >> /etc/sysctl.conf
