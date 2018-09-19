2b035eba5788432d5e006929d56d0afedcbbfed8
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-09-19 08:52:08 -0300

    add pgadmin4 standalone

---
+++ cli/commands/pgadmin.sh
@@ -0,0 +1,6 @@
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+izi get wget
+izi get ca-certificates
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add 
+izi up
+izi get pgadmin4
