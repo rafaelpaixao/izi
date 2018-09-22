162c92dbd388883a179055d092de24271eca1ce7
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-09-22 09:49:21 -0300

    rename python command to pyenv

--- cli/commands/pyenv.sh
+++ cli/commands/pyenv.sh
@@ -1,4 +1,5 @@
-# Bundle with Python 3.5.5 and 3.6.5 for PyEnv
+# Bundle with common Python3 dependencies and Python 3.5.5 and 3.6.5 for PyEnv
+
 izi get aria2
 izi get build-essential
 izi get curl
@@ -9,6 +10,8 @@ izi get libssl-dev
 izi get libbz2-dev
 izi get libsqlite3-dev
 izi get libpq-dev
+izi get python3-pip
+pip install --upgrade pip
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 printf 'export PATH="$HOME/.pyenv/bin:$PATH"\neval "$(pyenv init -)"\neval "$(pyenv virtualenv-init -)"\n' >> ~/.bashrc
 source $HOME/.bashrc
