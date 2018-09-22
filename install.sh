bd9493ac85141b842935a36627f854e69e30978a
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-09-22 09:54:34 -0300

    reload .bashrc after project installation

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ git clone https://github.com/rafaelpaixao/izi.git $HOME/.izi
 cp $HOME/.izi/cli/default.env $HOME/.izi/cli/.env
 echo "IZI_CLI=\$HOME/.izi/cli" >> $HOME/.bashrc
 echo ". \$IZI_CLI/master.sh" >> $HOME/.bashrc
+. $HOME/.bashrc
