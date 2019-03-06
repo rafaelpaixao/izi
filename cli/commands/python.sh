011b4bd2de10e5d374600c8d9a259228f034d4bb
Rafael Paixão <rafaelsspaixao@gmail.com>
2019-03-06 09:04:33 -0300

    Create python.sh

---
+++ cli/commands/python.sh
@@ -0,0 +1,14 @@
+izi get aria2
+izi get build-essential
+izi get curl
+izi get git
+izi get libreadline-dev
+izi get zlib1g-dev
+izi get libssl-dev
+izi get libbz2-dev
+izi get libsqlite3-dev
+izi get libpq-dev
+izi get python3-pip
+izi get python3-venv
+pip3 install --upgrade --user pip
+pip3 install --upgrade --user wheel
