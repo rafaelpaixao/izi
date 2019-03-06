b92763c9974c60309c2ea94201a401fa8df47751
Rafael Paixão <rafaelsspaixao@gmail.com>
2019-03-06 09:08:10 -0300

    Update pyenv.sh

--- cli/commands/pyenv.sh
+++ cli/commands/pyenv.sh
@@ -1,17 +1,6 @@
 # Bundle with common Python3 dependencies and Python 3.5.5 and 3.6.5 for PyEnv
 
-izi get aria2
-izi get build-essential
-izi get curl
-izi get git
-izi get libreadline-dev
-izi get zlib1g-dev
-izi get libssl-dev
-izi get libbz2-dev
-izi get libsqlite3-dev
-izi get libpq-dev
-izi get python3-pip
-pip3 install --upgrade pip
+izi python
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 printf 'export PATH="$HOME/.pyenv/bin:$PATH"\neval "$(pyenv init -)"\neval "$(pyenv virtualenv-init -)"\n' >> ~/.bashrc
 source $HOME/.bashrc
